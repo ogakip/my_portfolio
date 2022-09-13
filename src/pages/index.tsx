4f5a04a9a392e41c993ed0b441c3cd1ae25f7ade
ogaki <pauloogaki.principal@gmail.com>
2022-09-13 17:22:22 -0300

    change get window size function location

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 import { Button } from "../components/button";
 import HardSkills from "../components/skills";
 import { useState, useEffect } from "react";
-import useWindowSize from "./getWindowSize";
+import useWindowSize from "../utils/getWindowSize";
 
 const Home: NextPage = () => {
   const getWindowSize = useWindowSize();
